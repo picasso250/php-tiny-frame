3ed60ca0f508d12f172f09097210283351ba6733
Ryan <cumt.xiaochi@gmail.com>
2012-10-21 09:08:35 +0800

    when debug, open all error dispalys

--- index.php
+++ index.php
@@ -7,9 +7,20 @@
  * 此框架由王霄池纯粹手写而成，当然参照了不少鸡爷的框架，也参照了 LazyPHP
  */
 
+// 打开错误提示, SAE 可以 ini_set 将不起作用
+ini_set('display_errors', 1);
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
 define('IN_PTF', 1);
 
 require 'config/common.php';
+
+// if not debug, mute all error reportings
+if (!(defined('DEBUG') ? DEBUG : 0)) {
+	ini_set('display_errors', 0);
+	error_reporting(0);
+}
+
 require 'lib/function.php';
 
 // 变量初始化
