35ab1219aa5b46d195601913e53a62b2db8fa4c8
Ryan <cumt.xiaochi@gmail.com>
2013-10-02 10:39:41 +0800

    增加懒加载的服务

--- ptf/Controller.php
+++ ptf/Controller.php
@@ -3,11 +3,38 @@
 namespace ptf;
 
 /**
+ * 
+ * get set 可以设置懒加载的服务
  * @author ryan
  */
 class Controller
 {
     public $viewRoot;
+
+    private $vars;
+    private $lazies = array('names' => array(), 'values' => array());
+
+    public function __get($key)
+    {
+        if (array_key_exists($key, $this->vars)) {
+            return $this->vars[$key];
+        } elseif (array_key_exists($key, $this->lazies['names'])) {
+            if (!array_key_exists($key, $this->lazies['values'])) {
+                $this->lazies['values'][$key] = $this->lazies['names'][$key]();
+            }
+            return $this->lazies['values'][$key];
+        }
+        return null;
+    }
+
+    public function __set($key, $value)
+    {
+        if (is_callable($value)) {
+            $this->$lazies['names'][$key] = $value;
+        } else {
+            $this->$vars[$key] = $value;
+        }
+    }
 
     protected function params()
     {
