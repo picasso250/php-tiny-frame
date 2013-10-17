032ad836be7935cc4a16f0fc16cfec5ea2279ec2
Ryan <cumt.xiaochi@gmail.com>
2013-10-17 23:20:01 +0800

    增加注释

--- ptf/IdEntity.php
+++ ptf/IdEntity.php
@@ -16,13 +16,23 @@ class IdEntity
 
     protected $dirty = array();
 
+    /**
+     * 获取对应的Dao
+     * @return IdDao
+     */
     public static function dao()
     {
         $classname = get_called_class().'Dao';
         return new $classname;
     }
 
-    public static function make($model, $row)
+    /**
+     * 工厂函数，获取一个实例
+     * @param IdDao $model
+     * @param array $row
+     * @return IdEntity
+     */
+    public static function make($model, array $row)
     {
         $classname = get_called_class();
         $o = new $classname;
@@ -42,6 +52,10 @@ class IdEntity
         return $o;
     }
 
+    /**
+     * 获取主键的值
+     * @return int
+     */
     public function id()
     {
         $pkey = $this->model->pkey();
@@ -51,6 +65,10 @@ class IdEntity
         return $this->row[$pkey];
     }
 
+    /**
+     * 保存
+     * @return \ptf\IdEntity
+     */
     public function save()
     {
         if ($this->id()) {
@@ -62,6 +80,10 @@ class IdEntity
         return $this;
     }
 
+    /**
+     * 获取写脏的值，键值对数组
+     * @return array
+     */
     public function dirtyArray()
     {
         $set = array();
@@ -71,16 +93,28 @@ class IdEntity
         return $set;
     }
 
+    /**
+     * 标记为干净的
+     */
     public function clean()
     {
         $this->dirty = array();
+        return $this;
     }
 
+    /**
+     * 从数据库删除这条记录
+     * @return type
+     */
     public function delete()
     {
         return $this->model->delete($this);
     }
 
+    /**
+     * 转化为数组
+     * @return type
+     */
     public function toArray()
     {
         $num_args = func_num_args();
