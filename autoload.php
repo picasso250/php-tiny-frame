3fff1b60c25df2a01ded73264ecc2d93708368ad
picasso250 <cumt.xiaochi@gmail.com>
2013-10-20 12:34:22 +0800

    修复自动加载的bug

--- autoload.php
+++ autoload.php
@@ -1,5 +1,5 @@
 <?php
-spl_autoload_register(function () {
+spl_autoload_register(function ($name) {
     if (preg_match('/^ptf\b/', $name)) {
         require __DIR__.'/'.str_replace('\\', '/', $name).'.php';
     }
