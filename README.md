969c0eca69a43bd39c0b06d73c9dc5be9482b551
xiaochi <xiaochi@xc-CQ45-lp.(none)>
2013-10-20 11:24:56 +0800

    dos2unix  and 修改翻页类的游标机制

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 php-tiny-frame
 ==============
 
-Very small framework for website write in PHP.
+Small framework for website write in PHP.
 
 一个简单的 PHP 框架。使用 MVC 模式。支持 SAE 。
 
-（我猜很多 PHP 程序员都坐过这种事情，写了个小框架，然后慢慢放弃，转而使用别人的框架（比较大的）。或许这就是成长？）
+（我猜很多 PHP 程序员都做过这种事情，写了个小框架，然后慢慢放弃，转而使用别人的框架（比较大的）。或许这就是成长？）
 
 **我的目的**
 
--- ptf/lib/Paginate.php
+++ ptf/lib/Paginate.php
@@ -23,127 +23,69 @@ class Paginate {
     private $perPage = 10;  // 每页的条目数
     private $offset = null; // 
     private $viewNum = 5;  // 每个网页显示的最多的翻页格子
-    private $vernier = null; // 游标
 
-    function __construct($per_page=10, $total=0, $offset=0) {
+    private $start = 1; // 第一页
+    private $end; // 最后一页
+
+	private $previousHalfNum; // 前半翻页格子的个数
+    private $nextHalfNum; // 后半翻页格子的个数
+
+    public function __construct($perPage=10, $total=0, $offset=0) {
         if ($offset >= $total) $offset = 0;
-        $this->perPage = $per_page;
+        $this->perPage = $perPage;
         $this->total = $total;
         $this->offset = $offset;
+
+        // assume that all informations are given
+        $this->start = 1;
+        $this->end = ceil($total / $perPage);
+        // floor($this->total / $this->perPage - 0.00001) + 1;
+
+        $this->t_half = ($this->viewNum - 1) / 2;
+        $this->previousHalfNum = floor($t_half);
+        $this->nextHalfNum = ceil($t_half);
+
+        $this->showStart = $this->curPage - $this->viewNum;
+        $this->showEnd = $this->curPage + $this->viewNum;
+
     }
 
     public function setTotal($total) {
         $this->total = $total;
     }
 
-    public function setPerPage($per_page) {
-        $this->perPage = $per_page;
+    public function setPerPage($perPage) {
+        $this->perPage = $perPage;
     }
 
     public function setOffset($offset) {
         $this->offset = $offset;
     }
 
+    public function getPageNumbers() {
+        return range(max($this->showStart, $this->start), min($this->showEnd, $this->end));
+    }
+
     public function hasPrevious() {
-        return $this->offset > 0;
+        return $this->curPage > $this->start;
     }
 
     public function hasNext() {
-        return ($this->offset + $this->perPage) < $this->total; // ????
+        return $this->curPage < $this->end;
     }
 
-    public function previousOffset() {
-        $prev = $this->offset - $this->perPage;
-        return ($prev < 0)? 0 : $prev;
-    }
-
-    public function nextOffset() {
-        $next = $this->offset + $this->perPage;
-        return ($next > $this->total-1)? $this->total-1 : $next;
-    }
-    
     public function setViewNum($num) {
         $this->viewNum = $num;
     }
     
-    private function vernier() {
-        if ($this->vernier) 
-            return $this->vernier;
-        $minPage = 1;
-        $maxPage = $this->maxPage();
-        $curPage = $this->curPage();
-        $half = $this->viewNum / 2; // 游标的一半长度
-        $min = (int)($curPage - $half);
-        $max = (int)($curPage + $half);
-        
-        // 1. 当游标比整个尺子还大时
-        if ($this->viewNum >= $maxPage)
-            return array(
-                'reachStart' => false,
-                'reachEnd' => false,
-                'min' => $minPage,
-                'max' => $maxPage,
-            );
-        
-        $vernier = array(
-            'reachStart' => true,
-            'reachEnd' => true,
-            'min' => $min,
-            'max' => $max,
-        );
-        
-        // 2.a. 左边超出
-        if ($min <= $minPage) {
-            $vernier['reachStart'] = false;
-            $min = $minPage;
-            $max = $min + $this->viewNum - 1;
-        }
-        
-        // 2.b. 右边超出
-        if ($max >= $maxPage) {
-            $vernier['reachEnd'] = false;
-            $max = $maxPage;
-            $min = $max - $this->viewNum;
-        }
-        
-        $vernier['min'] = $min;
-        $vernier['max'] = $max;
-        $this->vernier = $vernier;
-        return $vernier;
+    public function isOmitStart() {
+        return $this->showStart > $this->start;
+    }
+    
+    public function isOmitEnd() {
+        return $this->showEnd < $this->end;
     }
 
-    public function viewMin() {
-        $v = $this->vernier();
-        return (int) $v['min'];
-    }
-    
-    public function reachStart() {
-        $v = $this->vernier();
-        return $v['reachStart'];
-    }
-    
-    public function viewMax() {
-        $v = $this->vernier();
-        return (int) $v['max'];
-    }
-    
-    public function reachEnd() {
-        $v = $this->vernier();
-        return $v['reachEnd'];
-    }
-    
-    public function isCurrent($page) {
-        return $page === $this->curPage();
-    }
-    
-    public function offset($i = -1) {
-        if ($i === -1) {
-            return $this->offset;
-        } else {
-            return $this->offset + $i * $this->perPage;
-        }
-    }
-    
     public function setCurPage($page) 
     {
         $this->curPage = $page;
@@ -157,27 +99,16 @@ class Paginate {
         return $this->offset / $this->perPage + 1;
     }
     
-    public function maxPage() {
-        return floor($this->total / $this->perPage - 0.00001) + 1;
+    public function getEnd() {
+        return $this->end;
     }
 
-    public function hrefPrefix()
+    public function hrefPrefix($pageKey = 'p')
     {
         $q = $_SERVER['QUERY_STRING'];
-        $arr = explode('&', $q);
-        $arr = array_filter($arr, function ($e) {
-            return strpos($e, 'p=') !== 0;
-        });
-        return implode('&amp;', $arr) . '&amp;p=';
+        $parse_str($q, $arr);
+        unset($arr[$pageKey]);
+        return http_build_query($arr);
     }
 
-    public function startCanShow()
-    {
-        return $this->viewMin() === 1;
-    }
-
-    public function endCanShow()
-    {
-        return $this->viewMax() === $this->maxPage();
-    }
 }
