4f98ef248ffd9c35ea9bc4de2f0b5655d04c70d3
wangxiaochi <wangxiaochi@baidu.com>
2014-11-18 16:50:48 +0800

    make it simple, worse is better

--- ptf/Application.php
+++ ptf/Application.php
@@ -13,55 +13,11 @@ use ptf\Router;
  */
 class Application
 {
-    public $root;
-
-    private $config = array();
-
-    public function config($config)
-    {
-        $this->config = $config;
-    }
-
     /**
-     * 初始化
-     * @return type
+     * ['GET', '%^get/(\d+)$%', 'Index', 'getItem']
+     * @var $rules
      */
-    public function init()
-    {
-        ob_start();
-        session_start();
-        date_default_timezone_set('PRC');
-
-        if ($this->root === null) {
-            $this->root = dirname(__DIR__);
-        }
-        $root = $this->root;
-
-        // auto require when using class (model)
-        spl_autoload_register(function ($classname) use ($root) {
-            $file_name = str_replace('\\', '/', $classname) . '.php';
-
-            if (preg_match('/Dao$/', $classname)) {
-                $file = "$root/dao/$file_name";
-                require $file;
-                return;
-            }
-
-            if (preg_match('/Controller$/', $classname)) {
-                $controller_file = "$root/controller/$file_name";
-                require $controller_file;
-                return;
-            }
-
-            $file = "$root/entity/$file_name";
-            if (file_exists($file)) {
-                require $file;
-            }
-        });
-
-        $this->router = new Router();
-        $this->router->rules($this->config['routers']);
-    }
+    public $rules = array();
 
     /**
      * 运行框架
@@ -69,26 +25,41 @@ class Application
      */
     public function run()
     {
-        $this->init();
+        $uri = $this->getRequestUri();
 
-        $req_uri = $this->getRequestUri();
-
-        list($call, $param) = $this->router->dispatch($req_uri);
-        if (is_array($call)) {
-            $class = $call[0].'Controller';
-            $func = $call[1].'Action';
-            $this->view_root = "$this->root/view";
-            $c = new $class($this);
-            if (method_exists($c, 'init')) {
-                $c->init();
+        $params = array();
+        if ($this->rules) {
+            // 解析规则（阻断性）
+            foreach ($this->rules as $rule) {
+                if ($this->macthMethod($rule[0]) && preg_match($rule[1], $url, $params)) {
+                    $controller = $rule[2];
+                    $action = $rule[3];
+                    break;
+                }
             }
-            foreach ($param as $key => $value) {
-                $c->$key = $value;
-            }
-            return $c->{$func}();
         } else {
-            return $call($param);
+            $controller = 'index';
+            $action = 'code404';
+            $params = [];
         }
+
+        $class = $controller.'Controller';
+        $func = $action.'Action';
+        $c = new $class();
+        if (method_exists($c, 'init')) {
+            $c->init();
+        }
+        return $c->{$func}($params);
+    }
+
+    public function macthMethod($method) {
+        if ($method == NULL) {
+            return TRUE;
+        }
+        if ($_SERVER['REQUEST_METHOD'] == $method) {
+            return TRUE;
+        }
+        return false;
     }
 
     // write file content to dst
@@ -96,30 +67,5 @@ class Application
         $arr = explode('?', $_SERVER['REQUEST_URI']);
         return $arr[0];
     }
-    
-    protected function mkdir($dir)
-    {
-        if (!file_exists($dir)) {
-            $rs = mkdir($dir, 0777, true);
-            if (!$rs) {
-                throw new Exception("unable to mkdir $dir", 1);
-            }
-            return $rs;
-        }
-        return true;
-    }
 
-    /**
-     * 将文件内容写到到SAE
-     * @param string $domain 域
-     * @param string $content 文件内容
-     * @param string $file_name 文件名
-     * @return string 上传后的地址
-     */
-    public function putContentsToSae($domain, $content, $file_name = null)
-    {
-        $s = new SaeStorage();
-        $s->write($domain , $file_name , $content);
-        return $s->getUrl($domain ,$file_name);
-    }
 }
